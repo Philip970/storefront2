81fe4ec6a6b71438c0b2b4e743fbcc074608becd
Philip970 <philip.ewane97@gmail.com>
2025-10-30 13:56:15 +0100

    3-7 Getting a Cart

--- store/views.py
+++ store/views.py
@@ -8,8 +8,8 @@ from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveM
 #from rest_framework.pagination import PageNumberPagination
 from .filters import ProductFilter
 from .pagination import DefaultPagination
-from .models import OrderItem, Product, Collection, Review, Cart
-from .serializers import CartSerializer, ProductSerializer, CollectionSerializer, ReviewSerializer
+from .models import CartItem, OrderItem, Product, Collection, Review, Cart
+from .serializers import CartItemSerializer, CartSerializer, ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -58,3 +58,12 @@ class ReviewViewSet(ModelViewSet):
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin,  GenericViewSet):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
+
+class CartItemViewSet(ModelViewSet):
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
+    
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}

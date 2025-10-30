5a3780ceb846af2aafe78f06c8338af3e401a44a
Philip970 <philip.ewane97@gmail.com>
2025-10-30 11:17:15 +0100

    2-12 searching

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models.aggregates import Count
-
+from rest_framework.filters import SearchFilter
 from .filters import ProductFilter
 from .models import OrderItem, Product, Collection, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
@@ -12,8 +12,9 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
 
     def get_serializer_context(self):
         return {'request': self.request}

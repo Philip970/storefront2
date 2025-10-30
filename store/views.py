128435312011a0e2a2c606f1575517a333c6aca7
Philip970 <philip.ewane97@gmail.com>
2025-10-30 11:31:17 +0100

    2-14 Pagination

--- store/views.py
+++ store/views.py
@@ -1,10 +1,12 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models.aggregates import Count
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from django_filters.rest_framework import DjangoFilterBackend
-from django.db.models.aggregates import Count
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 from .filters import ProductFilter
+from .pagination import DefaultPagination
 from .models import OrderItem, Product, Collection, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
@@ -14,6 +16,8 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    #pagination_class = PageNumberPagination
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
 

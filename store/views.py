4cfbf9602f7984379635aacd59adc69e429494e1
Philip970 <philip.ewane97@gmail.com>
2025-11-05 13:27:42 +0100

    6-13 applying model permissions

--- store/views.py
+++ store/views.py
@@ -4,12 +4,12 @@ from django.db.models.aggregates import Count
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, DjangoModelPermissions
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
 
-from store.permissions import IsAdminOrReadOnly
+from store.permissions import FullDjangoModelPermissions, IsAdminOrReadOnly
 #from rest_framework.pagination import PageNumberPagination
 from .filters import ProductFilter
 from .pagination import DefaultPagination
@@ -86,6 +86,7 @@ class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [IsAdminUser]
+    #permission_classes = [FullDjangoModelPermissions]
 
     """ def get_permissions(self):
         if self.request.method == 'GET':
